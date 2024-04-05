258a3d78012f7b9bee4b964ca3f12c7988395699
beedleye <chigatulia@gmail.com>
2024-04-06 01:14:02 +0300

    implimrenting postgre

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,42 @@ import (
 
 	"github.com/higatu/todo-app"
 	"github.com/higatu/todo-app/pkg/handler"
+	"github.com/higatu/todo-app/pkg/repository"
+	"github.com/higatu/todo-app/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDb(repository.Config{
+		Host: viper.GetString("db.host"), 
+		Port: viper.GetString("db.port"), 
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBname: viper.GetString("db.dbname"), 
+		SSLMode: viper.GetString("db.sslmode"),
+	})
+
+	if err != nil{
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
